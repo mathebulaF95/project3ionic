90e9a71915520edb5c0eac2c0d9b7b2a3dc9061c
mathebulaF95 <mathebulaF95@gmail.com>
2018-06-28 13:42:35 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  name;
+  quantity:number;
+  submit : boolean = false;
 
+  
+  constructor(public navCtrl: NavController) {
+   this.quantity = 0;
   }
+
+  
+
+  OnClick(num : number){
+    if(num == 1){
+      this.quantity = this.quantity + 1;
+  }
+}
+
+
+Click(num : number){
+  if(num == 0){
+    this.quantity = this.quantity - 1;
+}
+}
+
+Clickr(){
+  this.quantity = 0;
+ }
+
+ Clicko(){
+  this.submit = true;
+ }
 
 }
